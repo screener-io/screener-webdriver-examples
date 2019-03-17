041e5071782222bd78878f4d75992143d3c55868
Loyal Chow <loyalchow@gmail.com>
2019-03-17 00:16:20 -0700

    update webdriver and nightwatch examples with @screener.end

--- nightwatch/tests/basic.js
+++ nightwatch/tests/basic.js
@@ -1,9 +1,14 @@
 module.exports = {
-  'Nightwatch Example' : function (browser) {
+  'Nightwatch Example': browser => {
     browser
       .url('https://screener.io')
       .execute('/*@screener.init*/', ['Chrome: Nightwatch Test'])
       .execute('/*@screener.snapshot*/', ['Home'])
+      .execute('/*@screener.end*/', [], ({value}) => {
+        // @screener.end will return screener results
+        // Note: @screener.end will close your test session, so it must be called just before the end of your test
+        browser.assert.ifError(value.message);
+      })
       .end();
   }
 };
--- webdriverio/specs/spec.js
+++ webdriverio/specs/spec.js
@@ -1,7 +1,14 @@
-describe('WebdriverIO Example', function() {
-    it('should take snapshot of homepage', function () {
-        browser.url('https://screener.io');
-        browser.execute('/*@screener.init*/', 'Chrome: WebdriverIO Test');
-        browser.execute('/*@screener.snapshot*/', 'Home');
-    });
+const assert = require('assert');
+
+describe('WebdriverIO Example', () => {
+  it('should take snapshot of homepage', () => {
+    browser.url('https://screener.io');
+    browser.execute('/*@screener.init*/', 'Chrome: WebdriverIO Test');
+    browser.execute('/*@screener.snapshot*/', 'Home');
+    
+    // @screener.end will return screener results
+    // Note: @screener.end will close your test session, so it must be called just before the end of your test
+    const {value} = browser.execute('/*@screener.end*/');
+    assert.ifError(value.message);
+  });
 });
