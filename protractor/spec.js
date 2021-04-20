3bed49cfec80736eb56f17112b51f8182943fe19
Alex Griffen <alex.griffen@saucelabs.com>
2021-04-20 16:59:56 -0700

    Update spec.js with current Sauce Visual commands

--- protractor/spec.js
+++ protractor/spec.js
@@ -1,6 +1,7 @@
 describe('Protractor Example', function() {
   it('should take snapshot of homepage', function() {
+    browser.driver.executeScript('/*@visual.init*/', 'My Visual Test');
     browser.driver.get('https://screener.io');
-    browser.driver.executeScript('/*@screener.snapshot*/', 'Home');
+    browser.driver.executeScript('/*@visual.snapshot*/', 'Home');
   });
 });
